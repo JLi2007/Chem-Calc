5c1a18d205750c8647076c9f20b520a2e041345c
Mista7 <87041832+Mista7@users.noreply.github.com>
2024-09-23 23:07:21 -0400

    Logic Fixes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from table import periodic_table as pt
 import re
 
-findElements = r"[A-Z][a-z]?\d*"
+findElements = r"[A-Z][a-z]?\d?"
 findMultiplier = r'^\d+'
 
 st.write("Tanish Munjal")
@@ -11,11 +11,15 @@ compound = st.text_input("Enter your compound:", "")
 def parseCompound(c):
     elements = {}
     c = c.split("*")
-    # st.write(c)
     elems = re.findall(findElements, c[0]) #H2
-    elemMult = int(re.findall(findMultiplier, c[0])[0]) #7
-    hydrateMult = int(re.findall(findMultiplier, c[1])[0]) #7
-    hydrates = re.findall(findElements, c[1])
+    elemMult = 1 if re.findall(findMultiplier, c[0]) == [] else int(re.findall(findMultiplier, c[0])[0])#7
+    
+    try:
+        hydrates = re.findall(findElements, c[1])
+        hydrateMult =  1 if re.findall(findMultiplier, c[1]) == [] else int(re.findall(findMultiplier, c[1])[0])#7
+    except:
+        hydrateMult = 1
+        hydrates = []
     
     for i in elems:
         e = re.split(r"\d", i)[0]
@@ -25,7 +29,11 @@ def parseCompound(c):
         else:
             num = int(num)
 
-        elements[e] = num * elemMult
+        if e in elements:
+            elements[e] += num * elemMult
+        else:
+            elements[e] = num * elemMult
+        
     
     for i in hydrates:
         e = re.split(r"\d", i)[0]
@@ -35,18 +43,26 @@ def parseCompound(c):
         else:
             num = int(num)
 
-        elements[e] = num * hydrateMult
-    
+        if e in elements:
+            elements[e] += num * hydrateMult
+        else:
+            elements[e] = num * hydrateMult
+        
     return elements
- 
+
 def calcMm(c):
     elems = parseCompound(c)
     mm = 0
-    for i in elems:
-        mm += pt[i] * elems[i]
-    return mm
-        
+    st.write(elems)
+    try:
+        for i in elems:
+            mm += pt[i] * elems[i]
+        return mm
+    except:
+        st.write("Monkey, not all of them are real elements")
+        return "Monkey"
 
+    
 
 if(compound != ""):
     st.write(f"Molar mass = {calcMm(compound)}")
